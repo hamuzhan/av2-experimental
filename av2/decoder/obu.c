5ab66ce4b5f62fac192d3e2ba5c024c63d1e23aa
Wan-Teh Chang <wtc@google.com>
2026-02-05 14:13:12 -0800

    Validate seq_max_mlayer_cnt and set default value
    Fixes concern B and concern C in
    https://gitlab.com/AOMediaCodec/avm/-/issues/1284.

--- av2/decoder/obu.c
+++ av2/decoder/obu.c
@@ -442,6 +442,15 @@ static uint32_t read_sequence_header_obu(AV2Decoder *pbi,
     if (seq_params->max_mlayer_id > 0) {
       int n = avm_ceil_log2(seq_params->max_mlayer_id + 1);
       seq_params->seq_max_mlayer_cnt = avm_rb_read_literal(rb, n);
+      if (seq_params->seq_max_mlayer_cnt < 1 ||
+          seq_params->seq_max_mlayer_cnt > seq_params->max_mlayer_id + 1) {
+        avm_internal_error(
+            &cm->error, AVM_CODEC_UNSUP_BITSTREAM,
+            "seq_max_mlayer_cnt %d is outside the range of 1 to %d",
+            seq_params->seq_max_mlayer_cnt, seq_params->max_mlayer_id + 1);
+      }
+    } else {
+      seq_params->seq_max_mlayer_cnt = 1;
     }
 #endif  // CONFIG_AV2_PROFILES
   }
