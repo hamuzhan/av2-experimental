1867c65df984c0260556788b6390881bfc35057b
Andrew Segall <asegall@amazon.com>
2026-02-21 12:40:19 -0800

    Fix xlayer_id not being saved in show_existing_frame
    The code in read_show_existing_frame() and update_ref_frames_info()
    was saving the embedded layer ID (mlayer_id) but not the extended
    layer ID (xlayer_id). This caused problems when decoding streams
    with multiple extended layers, as stale xlayer_id values would be used.
    
    Fixes issue #1311

--- av2/decoder/decodeframe.c
+++ av2/decoder/decodeframe.c
@@ -6957,6 +6957,7 @@ static int read_show_existing_frame(AV2Decoder *pbi, bool is_regular_obu,
   cm->sef_ref_fb_idx = existing_frame_idx;
   cm->derive_sef_order_hint = avm_rb_read_bit(rb);
   cm->cur_frame->mlayer_id = cm->mlayer_id;
+  cm->cur_frame->xlayer_id = cm->xlayer_id;
 
   if (!cm->derive_sef_order_hint) {
     current_frame->order_hint = avm_rb_read_literal(
@@ -8417,6 +8418,7 @@ static int read_uncompressed_header(AV2Decoder *pbi, OBU_TYPE obu_type,
 
   cm->cur_frame->frame_type = current_frame->frame_type;
   cm->cur_frame->mlayer_id = cm->mlayer_id;
+  cm->cur_frame->xlayer_id = cm->xlayer_id;
   validate_refereces(pbi);
 
   cm->cur_frame->buf.bit_depth = seq_params->bit_depth;
